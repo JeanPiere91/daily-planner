51dff4338945311d44139add9f0933b2229fdeae
Jean Piere Bellota Mendoza <jeanpiere.bellota@gmail.com>
2023-12-20 00:27:16 +1100

    Added presentation with timeblocks for standar business hours

--- script.js
+++ script.js
@@ -3,8 +3,11 @@
 // in the html.
 dayjs.extend(dayjs_plugin_advancedFormat);
 var pEl = $('#currentDay');
+var divContainerEl = $('.container-lg');
 
-pEl.text(dayjs().format('dddd, MMMM Do'));
+var startHour = 9;
+var endHour = 17;
+var currentHour = 13; //dayjs().hour();
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -20,9 +23,59 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+
+  for (var i = 0 ; i < (endHour - startHour) + 1; i++) {
+    var hour = (i + startHour);
+    
+    var divEl = $('<div>');
+    divEl.attr('id', 'hour-' + hour);
+    divEl.addClass('row time-block');
+    
+    /* Added class acording to current-hour and hour-displayed */
+    if(hour < currentHour)
+      divEl.addClass('past');
+    else if (hour === currentHour)
+      divEl.addClass('present');
+    else
+      divEl.addClass('future');
+
+    /* Child Div */
+    var divEl1 = $('<div>');
+    divEl1.addClass('col-2 col-md-1 hour text-center py-3');
+    divEl1.text(hour + " " + dayjs().hour(hour).format('A'));
+    
+    /* Child textarea */
+    var taEl = $('<textarea>');
+    taEl.addClass('col-8 col-md-10 description');
+    taEl.attr('rows', '3');
+
+    /* Child button */
+    var btEl = $('<button>');
+    btEl.addClass('btn saveBtn col-2 col-md-1');
+    btEl.attr('aria-label', 'save');
+
+    /* button's child (i) */
+    var iEl = $('<i>');
+    iEl.addClass('fas fa-save');
+    iEl.attr('aria-hidden', 'true');
+
+    /* Added element i within button element */
+    btEl.append(iEl);
+
+    /* Added element div, textare, button within div element */
+    divEl.append(divEl1);
+    divEl.append(taEl);
+    divEl.append(btEl);
+
+    /* Added element div within div-container element */
+    divContainerEl.append(divEl);
+  }
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
+
   // TODO: Add code to display the current date in the header of the page.
+  pEl.text(dayjs().format('dddd, MMMM Do'));
 });
