13f833445c147cd216f615e12fe7f8507003313a
Jean Piere Bellota Mendoza <jeanpiere.bellota@gmail.com>
2023-12-21 23:48:18 +1100

    allow to save and edit appointments on LocalStorage

--- script.js
+++ script.js
@@ -4,10 +4,9 @@
 dayjs.extend(dayjs_plugin_advancedFormat);
 var pEl = $('#currentDay');
 var divContainerEl = $('.container-lg');
-
+var pMessageEl = $('#message');
 var startHour = 9;
 var endHour = 17;
-var currentHour = 13; //dayjs().hour();
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -17,12 +16,96 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  
+  // Reads tasks from local storage and returns array of task objects.
+  // Returns an empty array ([]) if there aren't any tasks.
+  function readTasksFromStorage() {
+    var tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      tasks = JSON.parse(tasks);
+    } else {
+      tasks = [];
+    }
+    return tasks;
+  }
+
+  function saveTasksToStorage(tasks) {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  // Gets project data from local storage and displays it
+  function printProjectData() { 
+    // get projects from localStorage
+    var tasks = readTasksFromStorage();
+    
+    // loop through each project and create a row
+    for (var i = 0; i < tasks.length; i += 1) {
+      $("#hour-"+ tasks[i].hour).children('textarea').val(tasks[i].description);
+    }
+  }
+
+  function handleTask(event) {
+
+    
+    //get element targeted 
+    var iClicked = $(event.target);
+    
+    /* Valid if element target is button or Icon, otherwise won't do anything */
+    if(event.target.tagName.toUpperCase().trim() === "BUTTON" || event.target.tagName.toUpperCase().trim() === "I")
+    {
+
+      // get identifier to save locally (hour)
+      var taskHour = iClicked.parents('div').eq(0).attr('id').split('-')[1];
+
+      // get description
+      var taskDescription = $('#hour-'+ taskHour).children('textarea').val();
+      
+      // add project to local storage
+      var tasks = readTasksFromStorage();
+
+      var newTask = {
+        hour: taskHour,
+        description: taskDescription,
+      };
+
+      /* Flag value to know if the task has been updated */
+      var editedValue = false;
+
+      /* looking if the task exists */
+      for(var i = 0; i < tasks.length; i++) {
+
+        /* if the value exist just update and mark that the value is updated  */
+        if(tasks[i].hour === taskHour)
+        {
+          tasks[i].description = taskDescription;
+          editedValue = true;
+        }
+      }
+
+      /* if the value is not updated, add a new task object */
+      if(!editedValue) {
+        alert("Appointment Added to localStorage");
+        tasks.push(newTask);
+      }
+      else
+        alert("Appointment Updated on localStorage");
+
+      saveTasksToStorage(tasks); 
+
+    }
+    else
+      return
+  }
+
+  divContainerEl.on('click', '.time-block', handleTask);
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+  var currentHour = 13; //dayjs().hour();
 
   for (var i = 0 ; i < (endHour - startHour) + 1; i++) {
     var hour = (i + startHour);
@@ -78,4 +161,6 @@ $(function () {
 
   // TODO: Add code to display the current date in the header of the page.
   pEl.text(dayjs().format('dddd, MMMM Do'));
+
+  printProjectData();
 });
